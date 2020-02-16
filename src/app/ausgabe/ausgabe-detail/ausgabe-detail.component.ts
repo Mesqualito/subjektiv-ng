eefe7c4880a7a54585b6f48102209f909d27a8b3
Mesqualito <jochen@gebsattel.rocks>
2020-02-16 17:45:14 +0100

    Code-Improvisation (2), better navigation in ausgabe-detail.component.ts

--- src/app/ausgabe/ausgabe-detail/ausgabe-detail.component.ts
+++ src/app/ausgabe/ausgabe-detail/ausgabe-detail.component.ts
@@ -1,24 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Location} from '@angular/common';
 import {IAusgabe} from "../../shared/model/ausgabe.model";
 import {ActivatedRoute, Router} from "@angular/router";
 import {AusgabeService} from "../../shared/services/ausgabe.service";
-import {Observable} from "rxjs";
+import {combineLatest, Observable, of} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-ausgabe-detail',
   templateUrl: './ausgabe-detail.component.html',
   styleUrls: ['./ausgabe-detail.component.scss']
 })
-export class AusgabeDetailComponent {
+export class AusgabeDetailComponent implements OnInit {
 
   ausgabe$: Observable<IAusgabe>;
   ausgabe: IAusgabe;
   private _ausgabeId: number;
   maxAusgaben$: Observable<number>;
   private _maxNum: number;
-  isPrev: boolean;
-  isNext: boolean;
+  isPrev$: Observable<boolean>;
+  isNext$: Observable<boolean>;
   errorMsg = '';
 
   constructor(
@@ -28,8 +29,10 @@ export class AusgabeDetailComponent {
     private _location: Location) {
     this._route.paramMap.subscribe(
       params => this._ausgabeId = parseInt(params.get('ausgabeId')));
-    this.ausgabe$ = this.getAusgabe(this._ausgabeId);
     this.getMaxAusgaben();
+  }
+
+  ngOnInit(): void {
     this.checkNavigation();
   }
 
@@ -46,17 +49,15 @@ export class AusgabeDetailComponent {
   checkNavigation(): void {
     this.ausgabe$ = this.getAusgabe(this._ausgabeId);
     this._location.replaceState('ausgabe/' + this._ausgabeId + '/view');
-    if (this._ausgabeId < this._maxNum) {
-      this.isNext = true;
-    }
     if (this._ausgabeId > 1) {
-      this.isPrev = true;
+      this.isPrev$ = of(true);
     } else {
-      this.isPrev = false;
+      this.isPrev$ = of(false);
     }
-
-    if (this._ausgabeId >= this._maxNum) {
-      this.isNext = false;
+    if (this._ausgabeId < this._maxNum) {
+      this.isNext$ = of(true);
+    } else {
+      this.isNext$ = of(false);
     }
   }
 
@@ -65,7 +66,7 @@ export class AusgabeDetailComponent {
       this._ausgabeId = this._ausgabeId - 1;
       this.checkNavigation();
     } else {
-      this.isPrev = false;
+      this.isPrev$ = of(false);
     }
   }
 
@@ -75,8 +76,14 @@ export class AusgabeDetailComponent {
       this._ausgabeId = this._ausgabeId + 1;
       this.checkNavigation();
     } else {
-      this.isNext = false;
+      this.isNext$ = of(false);
     }
+  }
+
+  get isAny$(): Observable<boolean> {
+    return combineLatest(this.isPrev$, this.isNext$).pipe(
+        map(([isPrev, isNext] ) => (isPrev || isNext))
+    );
   }
 }
 
